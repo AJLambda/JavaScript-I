91bed93112ac54f36faf1af1907a20d6f3c72c98
AJLambda <apj2277@gmail.com>
2019-02-25 17:49:03 -0800

    Added template literals

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,13 +63,13 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(inventory[32].car_year, inventory[32].car_make, inventory[32].car_model);
+console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${inventory[32].car_model}`);
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
 let lastCar = inventory.length - 1;
 
-console.log(inventory[lastCar].car_make, inventory[lastCar].car_model);
+console.log(`The last car in inventory is a ${inventory[lastCar].car_make} ${inventory[lastCar].car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
@@ -123,7 +123,7 @@ for (let i = 0; i < inventory.length; i++) {
         BMWAndAudi.push(inventory[i]);
     }
 }
-console.log(JSON.stringify(BMWAndAudi));
+console.log("A list of all the BMW and Audi cars on the lot:" + JSON.stringify(BMWAndAudi));
 
 
 
